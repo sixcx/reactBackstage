c3279fa2cbe7b7c3b1bd54fcc2ce3e5cb129b1a1
winskria <winskria@winskriadeMacBook-Pro.local>
2018-05-08 20:30:46 +0800

    basic component

--- src/utils/menu.js
+++ src/utils/menu.js
@@ -3,5 +3,11 @@ export const menus = [
     name: '首页',
     url: 'index',
     icon: 'home'
+  },
+  {
+    name: '基础组件', url: 'basic', icon: 'appstore-o',
+    sub: [
+      {name: '按钮', url: 'basic/button', icon: ''}
+    ]
   }
 ]
