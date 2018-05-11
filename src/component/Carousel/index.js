20a9c17c619b7ab74130c28d6412c17b2bd2bf66
winskria <winskria@winskriadeMacBook-Pro.local>
2018-05-11 17:44:35 +0800

    carousel component

---
+++ src/component/Carousel/index.js
@@ -0,0 +1,23 @@
+//轮播图组件
+
+import React from 'react'
+import './index.less'
+
+class Carousel extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      
+    }
+  }
+
+  render(){
+    return (
+      <div>
+
+      </div>
+    )
+  }
+}
+
+export default Carousel
--- src/pages/basic/carousel.js
+++ src/pages/basic/carousel.js
@@ -1,6 +1,15 @@
 //走马灯组件
 
 import React from 'react'
+import { Row, Col} from 'antd';
+import BannerAnim from 'rc-banner-anim';
+import QueueAnim from 'rc-queue-anim';
+import TweenOne from 'rc-tween-one';
+import './carousel.less'
+import './slider.less'
+
+const { Element } = BannerAnim;
+const BgElement = Element.BgElement;
 
 class Carousels extends React.Component {
   constructor(props) {
@@ -13,7 +22,48 @@ class Carousels extends React.Component {
   render () {
     return (
       <div>
-        carousels
+        <BannerAnim prefixCls="banner-user">
+          <Element key="aaa"
+            prefixCls="banner-user-elem"
+          >
+            <BgElement
+              key="bg"
+              className="bg"
+              style={{
+                backgroundImage: 'url(https://os.alipayobjects.com/rmsportal/IhCNTqPpLeTNnwr.jpg)',
+                backgroundSize: 'cover',
+                backgroundPosition: 'center',
+              }}
+            />
+            <QueueAnim name="QueueAnim">
+              <h1 key="h1">Ant Motion Demo</h1>
+              <p key="p">Ant Motion Demo.Ant Motion Demo.Ant Motion Demo.Ant Motion Demo</p>
+            </QueueAnim>
+            <TweenOne animation={{ y: 50, opacity: 0, type: 'from', delay: 200 }} name="TweenOne1">
+              Ant Motion Demo.Ant Motion Demo
+            </TweenOne>
+          </Element>
+          <Element key="bbb"
+            prefixCls="banner-user-elem"
+          >
+            <BgElement
+              key="bg"
+              className="bg"
+              style={{
+                backgroundImage: 'url(https://os.alipayobjects.com/rmsportal/uaQVvDrCwryVlbb.jpg)',
+                backgroundSize: 'cover',
+                backgroundPosition: 'center',
+              }}
+            />
+            <QueueAnim name="QueueAnim">
+              <h1 key="h1">Ant Motion Demo</h1>
+              <p key="p">Ant Motion Demo.Ant Motion Demo.Ant Motion Demo.Ant Motion Demo</p>
+            </QueueAnim>
+            <TweenOne animation={{ y: 50, opacity: 0, type: 'from', delay: 200 }} name="TweenOne2">
+              Ant Motion Demo.Ant Motion Demo
+            </TweenOne>
+          </Element>
+        </BannerAnim>
       </div>
     )
   }
