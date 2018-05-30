2cb4e87db56329f74f59de721bc09fd9e2243948
winskria <winskria@winskriadeMacBook-Pro.local>
2018-05-30 18:21:17 +0800

    todolist

--- src/pages/utils/todoList.js
+++ src/pages/utils/todoList.js
@@ -1,5 +1,6 @@
 //待办事项页面
 import React from 'react'
+import { Checkbox } from 'antd'
 
 import './todoList.less'
 
@@ -20,16 +21,19 @@ class TodoList extends React.Component {
               <a style={{ color: active? '#f01414' : '#4d555d' }}>待办任务</a>
               <a style={{ color: active? '#f01414' : '#4d555d' }}>已完成任务</a>
             </div>
-            {/* <ul className='list-group'>
-              <li className='todo-list-li'>
-
-              </li>
-            </ul>
-            <form className='todo-add'>
-              <input className='todo-input' />
-              <button className='todo-btn'>添加任务</button>
-            </form> */}
           </div>
+          <ul className='list-group'>
+            <li className='todo-list-li'>
+              <Checkbox className='check-box'>
+                test
+              </Checkbox>
+              <button className='todo-list-del'>删除</button>
+            </li>
+          </ul>
+          <form className='todo-add'>
+            <input className='todo-input' />
+            <button type='submit' className='todo-btn'>添加任务</button>
+          </form>
         </div>
       </div>
     )
